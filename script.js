1c6fb72d64cd4edf06a10790631add93d725e3ec
Adnan Hussain <adnanrajput42@gmail.com>
2022-03-15 21:18:48 +0500

    fix select a choice

--- script.js
+++ script.js
@@ -2,27 +2,32 @@
     {
         question: "What is 2*5?",
         choices: [2, 5, 10, 15, 20],
-        correctAnswer: 10
+        correctAnswer: 10,
+        userAnswer: -1
     }, 
     {
         question: "What is 6*10?",
         choices: [10, 30, 60, 80, 100],
-        correctAnswer: 60
+        correctAnswer: 60,
+        userAnswer: -1
     }, 
     {
         question: "What is 8*6?",
         choices: [34, 72, 48, 80, 99],
-        correctAnswer: 48
+        correctAnswer: 48,
+        userAnswer: -1
     }, 
     {
         question: "What is 9*4?",
         choices: [36, 55, 69, 77, 80],
-        correctAnswer: 36
+        correctAnswer: 36,
+        userAnswer: -1
     },
     {
         question: "What is 11*4?",
         choices: [33, 55, 88, 44, 99],
-        correctAnswer: 44
+        correctAnswer: 44,
+        userAnswer: -1
     }
 ];
 var questionIndex = 0;
@@ -42,7 +47,7 @@ function prepareChoices(questionIndex) {
     let newChoices = '';
     for (let index = 0; index < choices.length; index++) {
         const choice = choices[index];
-        newChoices = newChoices + '<div class="choice"><span>'+ choice +'</span></div>';
+        newChoices = newChoices + `<div class="choice ${index === question.userAnswer? 'selected' :''}"><span>${choice}</span></div>`;
     }
     return newChoices
 }
@@ -55,31 +60,33 @@ function showCurrentQuestion(questionIndex){
     const selectedChoices = document.querySelectorAll(".choice");
     console.log (selectedChoices);
     for(i=0; i < selectedChoices.length; i++){
-        selectedChoices[i].setAttribute("onclick", "selectedUserChoice(this)"); 
+        selectedChoices[i].setAttribute("onclick", `selectedUserChoice(${i})`); 
     }
 }
 
-function selectedUserChoice(answer)
+function selectedUserChoice(choosedChoiceIndex)
 {
+  console.log('selectedUserChoice==>',choosedChoiceIndex)
     //answer.classList.add("selected");
-    var userAnswer = answer.textContent;
     var currentQuestion = questions[questionIndex];
     var actualAnswer = currentQuestion.correctAnswer;
-    var allChoices = currentQuestion.choices.length;
+    var userAnswer = currentQuestion.choices[choosedChoiceIndex];
+    currentQuestion.userAnswer = choosedChoiceIndex;
+    // var allChoices = currentQuestion.choices.length;
 
-    for (let i=0; i<=allChoices; i++){
-        if (userAnswer==currentQuestion.choices[i]){
-            currentIndex = i;
-            break;
-        }
-    }
+    // for (let i=0; i<=allChoices; i++){
+    //     if (userAnswer==currentQuestion.choices[i]){
+    //         currentIndex = i;
+    //         break;
+    //     }
+    // }
     showCurrentQuestion(questionIndex);
     
-    while (currentQuestion.choices.length){
-        showCurrentQuestion(questionIndex);
-        console.log(currentQuestion.choices);
-    }
-    if (userAnswer==actualAnswer){
+    // while (currentQuestion.choices.length){
+    //     showCurrentQuestion(questionIndex);
+    //     console.log(currentQuestion.choices);
+    // }
+    if (userAnswer === actualAnswer){
         totalScore++;   
     }
 
